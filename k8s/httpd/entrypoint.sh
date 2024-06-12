183e3a3461f9f20c48850ede6c1a0d1a0021d480
kevinharv <kharvey2002@gmail.com>
2024-06-11 21:22:30 -0500

    updated containerization

--- k8s/httpd/entrypoint.sh
+++ k8s/httpd/entrypoint.sh
@@ -3,13 +3,15 @@
 # Exit if a command returns non-zero code
 set -e
 
-# Create logs directory
-    # mkdir
-    # chown
+# Ensure ownership of WebDAV lock DB
+chown -R www-data:www-data /var/lib/dav
+chmod 700 /var/lib/dav
 
-# chown DAV lock directory
-# chown conf directory
-# chown site contents
+# chown conf directory?
+
+# Ensure ownership of site content
+chown -R www-data:www-data /usr/local/apache2/htdocs
+chmod 700 /usr/local/apache2/htdocs
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
