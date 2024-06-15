ff3dd637adf853666eb0cc6860d0584353bd3cd7
kevinharv <kharvey2002@gmail.com>
2024-06-14 19:46:38 -0500

    wip: k8s httpd

--- k8s/httpd/entrypoint.sh
+++ k8s/httpd/entrypoint.sh
@@ -5,13 +5,15 @@ set -e
 
 # Ensure ownership of WebDAV lock DB
 chown -R www-data:www-data /var/lib/dav
-chmod 700 /var/lib/dav
+chmod -R 777 /var/lib/dav
 
-# chown conf directory?
+# Ensure ownership of configuration
+# chown -R www-data:www-data /usr/local/apache2/conf
+chmod -R o+r /usr/local/apache2/conf
 
 # Ensure ownership of site content
-chown -R www-data:www-data /usr/local/apache2/htdocs
-chmod 700 /usr/local/apache2/htdocs
+# chown -R www-data:www-data /usr/local/apache2/htdocs
+chmod -R o+rw /usr/local/apache2/htdocs
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /usr/local/apache2/logs/httpd.pid
